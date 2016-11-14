f669cffdf50b424ff1fd8864f6ceb8743dfbb6cf
ddashon <dasha-dinamit@yandex.ru>
2016-11-14 23:48:39 +0300

    Add files via upload
    hw3 added

---
+++ python hw3.py
@@ -0,0 +1,10 @@
+n=[]
+while True:
+    word = input ('word:')
+    if word ==(''):break
+    elif word.endswith ('tur'):
+        n.append(word)
+        print ('/n'. join(n))
+        
+
+        
