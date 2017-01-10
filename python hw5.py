2215d4a5e37ef63f609bf9b30d9a6c394b2652d5
ddashon <dasha-dinamit@yandex.ru>
2017-01-10 12:07:52 +0300

    hw 5 added with 6 .txt files

--- python hw5.py
+++ python hw5.py
@@ -1,13 +1,55 @@
-a=open (input(), 'r', encoding='utf-8')
-b=0
-c=0
-for line in a:
-    arr=line.split()
-    b=b+len(arr)
-    for d in arr:
-        if len(d)>10:
-            c=c+1
-            a.close()
-            e=c/b*100
-            print (e, '%')
+import random
+
+def noun ():
+    file=open('Mnoun.txt', 'r', encoding='utf-8')
+    f=readlines()
+    nouns=[]
+    for line in f:
+        nouns.append(line.split(" "))
+        return random.choise(nouns)
+def verb ():
+    file=open ('verb1.txt', 'r', encoding='utf-8')
+    f=readlines()
+    verbs=[]
+    for line in f:
+        verbs.append(line.split(" "))
+        return random.choise(verbs)
+def adj ():
+    file=open ('adj.txt', 'r', encoding='utf-8')
+    f=readlines()
+    adjectives=[]
+    for line in f:
+        adjectives.append(line.split(" "))
+        return random.choise(adjectives)
+
+def noun2 ():
+    file=open ('noun2.txt','r', encoding='utf-8')
+    f=readlines()
+    plnouns=[]
+    for line in f:
+        plnouns.append(line.split(" "))
+        return random.choise(plnouns)
+def conj():
+    conjs=["и", "или", "но", "да", "однако", "зато", "когда", "пока", "потому что", "чтобы", "то есть"]
+    return "," + random.choise(conjs)
+def noun3 ():
+    file=open ('noun3.txt', 'r', encoding='utf-8')
+    f=readlines ()
+    fnouns=[]
+    for line in f:
+        fnouns.append (line.split(" "))
+        return random.choise(fnouns)
+def 2verb ():
+    file=open ('2verb.txt', 'r', encoding ='utf-8')
+    f=readlines ()
+    2verbs=[]
+    for line in f:
+        2verbs.append (line.split(" "))
+        return random.choise (2verbs)
+def sen ():
+    return (noun+" "+verb+" "+adj+" "+noun2+" "+conj+" "+noun3+" "+2verb+"."
+
+for i in range(5):
+            print (sen())
+
         
