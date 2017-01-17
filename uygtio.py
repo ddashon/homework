9bd821074d3957546bebbcbcf0f4afc2af0e4f4e
ddashon <dasha-dinamit@yandex.ru>
2017-01-17 15:00:51 +0300

    jghg

---
+++ uygtio.py
@@ -0,0 +1,21 @@
+d={'Germany':'Berlin','France':'Paris',
+ 'USA':'Washington DC',
+ 'Russia':'Moscow'}
+for key in d:
+    print (key+"*"+d[key])
+
+def capital(a):
+    a=input("Country:")
+    if a in d:
+        return (d[a])    
+    else: print ("oops")
+
+def revert ():
+    k={}
+    countries=d.keys()
+    for key in countries:
+        k[d[key]]=key
+    return (k)
+a=revert()
+print(a)
+#cfdddddddddfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff;f;f;f;f;f;f;f;f;f;f;f;f;f;f;;f;f;f;;;;;f;f;f;f;f;;f;f;f;f;f;f;f;f;;f;f;f;f;f;f;f;f;f;f;;f;f;;f;f;f;;f;;f;f;f;;f;f;f;f;;f;f;f;f;f;f;;f;f;f;ffffffdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffddgfxwjd chgffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffgfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffdaddashadadadadadadadadadadadadadadadadadadadaadadadadnhd ntfjhgnhjgmgnjdashA
